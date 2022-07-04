7310a69ae62fba748f6665cae7a1925830d6e006
Maria Khristenko <14mashahr14@gmail.com>
2022-07-04 16:38:43 +0300

    algorithm 1.0

--- Algorithm.cpp
+++ Algorithm.cpp
@@ -1,1 +1,31 @@
 #include "Algorithm.h"
+
+Square::Square(const SquareType &new_type) : type_(new_type) {
+}
+
+Square::Square(const SquareType &new_type, size_t coin) : type_(new_type), coin_number_(coin) {
+}
+
+const SquareType& Square::GetType() {
+    return type_;
+}
+
+size_t Square::GetCoins() {
+    return coin_number_;
+}
+
+void Field::SetSquare(size_t x, size_t y, Square new_square) {
+    table_[x][y] = std::move(new_square);
+}
+
+const Square& Field::GetSquare(size_t x, size_t y) {
+    return table_[x][y];
+}
+
+Algorithm::Algorithm(game_parameters params) {
+    params_ = std::move(params);
+}
+
+std::pair<size_t, size_t> Algorithm::GetNextStep() {
+
+}
--- Algorithm.h
+++ Algorithm.h
@@ -1,6 +1,7 @@
+#pragma once
 #include "MsgProcess.h"
 
-enum class SquareState {
+enum class SquareType {
     Coin,
     Block,
     Empty,
@@ -8,24 +9,38 @@ enum class SquareState {
     MyBot
 };
 
+class Square {
+ public:
+    Square(const SquareType &new_type);
+    Square(const SquareType &new_type, size_t coin);
+    const SquareType& GetType();
+    size_t GetCoins();
+
+ private:
+    SquareType type_;
+    size_t coin_number_;
+};
+
 class Field
 {
  public:
     Field() = default;
-    void Set(size_t x, size_t y, SquareState new_state);
-    SquareState Get(size_t x, size_t y);
+    void SetSquare(size_t x, size_t y, Square new_square);
+    const Square& GetSquare(size_t x, size_t y);
 
  private:
-    std::vector<std::vector<SquareState>> table;
-    map_size table_size;
+    std::vector<std::vector<Square>> table_;
+    map_size table_size_;
 };
 
 class Algorithm
 {
  public:
-    Algorithm() = default;
+    Algorithm(game_parameters params);
+    void Set(size_t x, size_t y, Square new_square);
+    std::pair<size_t, size_t> GetNextStep();
 
  private:
-    game_parameters params;
-    Field field;
+    game_parameters params_;
+    Field field_;
 };
